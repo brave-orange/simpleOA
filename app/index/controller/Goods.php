3fb09be8b1dc634be090d9ba71db8db18962d3a5
guan <guanzejian@shanghai.com.cn>
2019-03-29 16:11:56 +0800

    商品列表

--- app/index/controller/Goods.php
+++ app/index/controller/Goods.php
@@ -7,4 +7,13 @@ class Goods extends Controller
 	public function goods_list(){
 		return view();
 	}
+	public function goods_specifications(){
+		return view();
+	}
+	public function goods_category(){
+		return view();
+	}
+	public function goods_manufacturer(){
+		return view();
+	}
 }
